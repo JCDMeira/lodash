54650034a800b21fb7ce734aba0e0304a839979a
Jean Meira <jean.meira10@hotmail.com>
2022-03-13 21:19:45 -0300

    feat: add map example

---
+++ map.js
@@ -0,0 +1,18 @@
+//_.map()
+const _ = require("lodash");
+
+const users = [
+  { name: "Tiago Matos", age: 20, gender: "male" },
+  { name: "Jon Snow", age: 18, gender: "male" },
+  { name: "Wonder woman", age: 22, gender: "female" },
+  { name: "Tiago Matos", age: 20, gender: "male" },
+  { name: "Jon Snow", age: 18, gender: "male" },
+  { name: "Wonder woman", age: 22, gender: "female" },
+  { name: "Tiago Matos", age: 20, gender: "male" },
+  { name: "Jon Snow", age: 18, gender: "male" },
+  { name: "Wonder woman", age: 22, gender: "female" },
+];
+
+const result = _.map(users, "name");
+
+console.log(result);
