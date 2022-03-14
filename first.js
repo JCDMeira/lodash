52dd02b3c7428d9a6b5088b0bbb1c83af11af57f
Jean Meira <jean.meira10@hotmail.com>
2022-03-14 14:59:55 -0300

    feat: add first  example

---
+++ first.js
@@ -0,0 +1,18 @@
+//_.first()
+const _ = require("lodash");
+
+const users = [
+  { name: "Tiago Matos", age: 20, gender: "male" },
+  { name: "Jon Snow", age: 18, gender: "male" },
+  { name: "Wonder woman", age: 22, gender: "female" },
+  { name: "Tiago Matos", age: 20, gender: "male" },
+  { name: "Jon Snow", age: 18, gender: "male" },
+  { name: "Wonder woman", age: 22, gender: "female" },
+  { name: "Tiago Matos", age: 20, gender: "male" },
+  { name: "Jon Snow", age: 18, gender: "male" },
+  { name: "Wonder woman", age: 22, gender: "female" },
+];
+
+const result = _.first(users);
+
+console.log(result);
